f2cda156e08186477d90bc8cc05a22629484bf6a
tsuny221 <kingoftsuny@gmail.com>
2020-09-22 13:44:50 +0900

    [Update] sample site practice

--- 98.5_サンプルサイト_練習用 /scripts/main.js
+++ 98.5_サンプルサイト_練習用 /scripts/main.js
@@ -1,5 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hero = new HeroSlider('.swiper-container');
-    hero.start();
-});
+document.addEventListener("DOMContentLoaded", function () {
+  const hero = new HeroSlider(".swiper-container");
+  hero.start();
 
+  const cb = function (el, isIntersecting) {
+    if (isIntersecting) {
+      const ta = new TweenTextAnimation(el);
+      ta.animate();
+    }
+  };
+  const so = new ScrollObserver(".tween-animate-title", cb);
+
+  const _inviewAnimation = function (el, inview) {
+    if (inview) {
+      el.classList.add("inview");
+    } else {
+      el.classList.remove("inview");
+    }
+  };
+  const so2 = new ScrollObserver(".cover-slide", _inviewAnimation);
+});
